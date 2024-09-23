3dedd80bb702adc73301457229b0e2d7cde80343
taramanji <131508158+JavaLangRuntimeException@users.noreply.github.com>
2024-09-23 13:58:29 +0900

    Update auto_input.sh

--- tools/auto_input.sh
+++ tools/auto_input.sh
@@ -1,40 +1,30 @@
 #!/bin/bash
+# - written by @aonrjp
 
 TYPES=("houshin" "soukatsu")
 SECTIONS=("kaikei" "kensui" "soumu" "syogai" "system" "zentai")
 
-for type in "${TYPES[@]}"
+for type in ${TYPES[@]}
 do
-    # 各タイプ（houshin, soukatsu）のメインファイルを作成
-    main_file="src/${type}.tex"
-    echo "\\documentclass{article}" > "$main_file"
-    echo "\\begin{document}" >> "$main_file"
-
-    for section in "${SECTIONS[@]}"
+    for section in ${SECTIONS[@]}
     do
         # 所定ディレクトリ以下に含まれるtexファイルを取得してソート
-        files=$(find "src/${type}/${section}" -type f -name "*.tex" | sort)
+        files=`find src/${type}/${section} -type f -name "*.tex" | sort`
         
         if [ "$1" = "show" ]; then
             # 見つかったtexファイルのパスを出力
             echo "${files}"
         else
-            # セクションごとにサブセクションを作成
-            echo "\\section{${section^}}" >> "$main_file"
+            # input文を生成 (シェルによって\\\\が\\となり、正規表現で\\は\を表す)
+            doc=`echo "${files}" | sed -e 's/^/\\\\input{/' | sed -e 's/$/}/'`
 
-            # input文を生成し、メインファイルに追加
-            if [ -n "$files" ]; then
-                while IFS= read -r file; do
-                    echo "\\input{${file}}" >> "$main_file"
-                done <<< "$files"
-            else
-                echo "% No .tex files found in ${type}/${section}" >> "$main_file"
+            # texファイルが発見できなければ虚無を
+            if [[ $files == '' ]]; then
+                doc=''
             fi
 
-            echo "" >> "$main_file"  # 空行を追加
+            # 書き込む
+            echo "${doc}" > src/${type}/${section}.tex
         fi
     done
-
-    # ドキュメントを閉じる
-    echo "\\end{document}" >> "$main_file"
 done
